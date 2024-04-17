509335dc9e674b3ed51f9c14297b19f05a9e873c
AbdAlrahman Hassan <abdalrahman.hassan237@gmail.com>
2024-04-17 17:33:34 +0200

    add oop project

---
+++ oop_Bank_account_projects.py
@@ -0,0 +1,20 @@
+from Bank_accounts import *
+
+# create objects of the BankAccount
+
+account1 = BankAccount(1000,"omar ali")
+account2 = BankAccount(4000,"yousef ahmed")
+
+# account1.get_balance()
+# account2.get_balance()
+
+# make withdraw  
+
+account1.withdraw(200)
+account2.withdraw(2000)
+
+# make deposit
+
+account1.deposit(1600)
+account2.deposit(8000)
+
